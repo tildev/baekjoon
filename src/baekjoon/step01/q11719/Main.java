708e0c6b779bfb126569913a3dca384808cd8e4d
tildev <tildevelove@gmail.com>
2018-07-18 10:48:39 +0900

    baekjoon.step01.q11719 풀이

---
+++ src/baekjoon/step01/q11719/Main.java
@@ -0,0 +1,22 @@
+package baekjoon.step01.q11719;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author tildev
+ * @data 2018. 7. 18.
+ */
+// 그대로 출력하기2
+// 메모리 제한, 시간 제한/ 256MB, 1초
+// 10616 KB, 76 MS	
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s;
+		while ((s = br.readLine()) != null) {
+			System.out.println(s);
+		}
+	}
+}

577d8e94359660d4d595f31976bf2d2686cd9ee9
tildev <tildevelove@gmail.com>
2018-07-18 10:13:13 +0900

    baekjoon.step01.q2557 풀이

---
+++ src/baekjoon/step01/q2557/Main.java
@@ -0,0 +1,14 @@
+package baekjoon.step01.q2557;
+
+/**
+ * @author tildev
+ * @data 2018. 7. 18.
+ */
+// Hello World
+// 메모리 제한, 시간 제한/ 128MB, 1초
+// 9748 KB, 72 MS
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}

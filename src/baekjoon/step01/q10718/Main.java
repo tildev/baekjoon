a68ec815cad6ee39d9e1979b9cf332c9730c783a
tildev <tildevelove@gmail.com>
2018-07-18 10:48:19 +0900

    baekjoon.step01.q10718 풀이

---
+++ src/baekjoon/step01/q10718/Main.java
@@ -0,0 +1,15 @@
+package baekjoon.step01.q10718;
+
+/**
+ * @author tildev
+ * @data 2018. 7. 18.
+ */
+// We love kriii 
+// 메모리 제한, 시간 제한/ 256MB, 1초
+// 9772 KB,	68 MS
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+	}
+}

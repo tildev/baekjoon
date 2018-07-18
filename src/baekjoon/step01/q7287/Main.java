f221bbe952b6462cdd84dfab54d11654c80df444
tildev <tildevelove@gmail.com>
2018-07-18 10:28:12 +0900

    baekjoon.step01.q7287 풀이

---
+++ src/baekjoon/step01/q7287/Main.java
@@ -0,0 +1,15 @@
+package baekjoon.step01.q7287;
+
+/**
+ * @author tildev
+ * @data 2018. 7. 18.
+ */
+// 등록
+// 메모리 제한, 시간 제한/ 128MB, 1초
+// 9752 KB, 72 MS
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("17");
+		System.out.println("hallo");
+	}
+}

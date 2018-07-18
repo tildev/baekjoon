d66272716da8eaad219135fc9fd18d3e4ad02dcd
tildev <tildevelove@gmail.com>
2018-07-18 10:48:07 +0900

    baekjoon.step01.q10172 풀이

---
+++ src/baekjoon/step01/q10172/Main.java
@@ -0,0 +1,18 @@
+package baekjoon.step01.q10172;
+
+/**
+ * @author tildev
+ * @data 2018. 7. 18.
+ */
+// 개
+// 메모리 제한, 시간 제한/ 256MB, 1초
+// 9876 KB, 68 MS
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+	}
+}

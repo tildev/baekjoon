6f308ee090b9a4f93b985f9e0a71a8845e19711c
tildev <tildevelove@gmail.com>
2018-09-25 17:35:37 +0900

    solve the problem 4344 of step04

---
+++ src/baekjoon/step04/q4344/Main.java
@@ -0,0 +1,44 @@
+package baekjoon.step04.q4344;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @author tildev
+ * @data 2018. 9. 25.
+ */
+// 평균은 넘겠지
+// 메모리 제한, 시간 제한/ 128MB, 1초
+// 11816 KB, 112 ms
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int cnt = Integer.valueOf(br.readLine());
+
+        int sdCnt;
+        double sdNum, avg;
+        for (int i = 0; i < cnt; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            
+            sdCnt = Integer.valueOf(st.nextToken());
+            int score[] = new int[sdCnt];
+            avg = 0;
+            for(int j =0; j<sdCnt; j++) {
+                score[j] = Integer.valueOf(st.nextToken());
+                avg += score[j];
+            }
+            avg = avg/(double)sdCnt;
+            sdNum = 0;
+            for(int j =0; j<sdCnt; j++) {
+                if(score[j]>avg) {
+                    sdNum +=1;
+                }
+            }
+            
+            System.out.printf("%.3f",(sdNum/(double)sdCnt*100.0));
+            System.out.println("%");
+        }
+    }
+}

5aaeb5d91a8a6ad803354b9ba31b5f7311796589
tildev <tildevelove@gmail.com>
2018-10-14 16:49:31 +0900

    solve the problem 1193 of setp08

---
+++ src/baekjoon/step08/q1193/Main.java
@@ -0,0 +1,28 @@
+package baekjoon.step08.q1193;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * @author tildev
+ * @date 2018. 10. 14.
+ */
+// 분수찾기
+// 메모리 제한, 시간 제한/ 128MB, 1초
+// 10020 KB, 76 ms
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int val = Integer.valueOf(br.readLine());
+        int cnt = 1;
+        while (val > cnt) {
+            val -= cnt;
+            cnt++;
+        }
+        if(cnt%2==0) {
+            System.out.println(val+"/"+(cnt+1-val));
+        }else {
+            System.out.println((cnt+1-val)+"/"+val);
+        }
+    }
+}

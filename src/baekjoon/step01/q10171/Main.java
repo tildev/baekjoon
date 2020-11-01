f1b69bcf77cf9d5840253d2ce3e44a6405f2001d
tildev <tildevelove@gmail.com>
2020-11-02 02:33:33 +0900

    Solve the problem 10171 of setp1

---
+++ src/baekjoon/step01/q10171/Main.java
@@ -0,0 +1,18 @@
+package baekjoon.step01.q10171;
+
+/**
+ * @author tildev
+ * @data 2020. 11. 02.
+ */
+// 고양이
+// 메모리 제한, 시간 제한/ 256MB, 1초
+// 14104 KB, 136 MS
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(
+                "\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+    }
+}

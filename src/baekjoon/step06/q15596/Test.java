b223dde88b24dcdebbb44861a58ca30f664c26c9
tildev <tildevelove@gmail.com>
2020-11-10 01:05:46 +0900

    Edit limit info

--- src/baekjoon/step06/q15596/Test.java
+++ src/baekjoon/step06/q15596/Test.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 /**
  * 정수 N개의 합
  * 메모리, 시간 제한  - 512 MB,   0.1초
- * 메모리, 시간      - 18768 KB, 232 MS
+ * 메모리, 시간      - 424900 KB, 188 MS
  *
  * @author tildev
  * @data 2020. 11. 10.

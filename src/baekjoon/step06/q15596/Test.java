fafde71f2dac6c6bc7cdeba5a3f893058792453b
tildev <tildevelove@gmail.com>
2020-11-10 01:04:32 +0900

    Solve the problem 15596 of step05

---
+++ src/baekjoon/step06/q15596/Test.java
@@ -0,0 +1,18 @@
+package baekjoon.step06.q15596;
+
+import java.util.Arrays;
+
+/**
+ * 정수 N개의 합
+ * 메모리, 시간 제한  - 512 MB,   0.1초
+ * 메모리, 시간      - 18768 KB, 232 MS
+ *
+ * @author tildev
+ * @data 2020. 11. 10.
+ */
+public class Test {
+
+    public long sum(int[] a) {
+        return Arrays.stream(a).asLongStream().sum();
+    }
+}

7019a86a6f2749016769085b3c350a38f27f35cf
tildev <tildevelove@gmail.com>
2020-12-06 20:39:07 +0900

    Solve the problem 1927 of step 21

---
+++ src/baekjoon/step21/q1927/Main.java
@@ -0,0 +1,39 @@
+package baekjoon.step21.q11279;
+
+import java.io.*;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+/**
+ * 최대 힙
+ * 메모리, 시간 제한  - 256 MB,   1초
+ * 메모리, 시간      - 26200 KB, 332 ms
+ *
+ * @author tildev
+ * @data 2020. 12. 06.
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if (x == 0) {
+                if (pq.size() == 0) {
+                    bw.write("0\n");
+                } else {
+                    bw.write(pq.poll() + "\n");
+                }
+            } else {
+                pq.add(x);
+            }
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}

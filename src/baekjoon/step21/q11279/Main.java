64f00c5264b09866ed0630ca683784ccb8abaae5
tildev <tildevelove@gmail.com>
2020-12-09 23:49:31 +0900

    Edit wrong package

--- src/baekjoon/step21/q11279/Main.java
+++ src/baekjoon/step21/q11279/Main.java
@@ -1,14 +1,13 @@
 package baekjoon.step21.q11279;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 /**
- * 가장 긴 증가하는 부분 수열 2
+ * 최대 힙
  * 메모리, 시간 제한  - 512 MB,   1초
- * 메모리, 시간      -  KB,  MS
+ * 메모리, 시간      - 26200 KB, 332 ms
  *
  * @author tildev
  * @data 2020. 12. 05.
@@ -16,31 +15,25 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] a = new int[n];
-        int[] cnt = new int[n];
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
 
         for (int i = 0; i < n; i++) {
-            a[i] = Integer.parseInt(st.nextToken());
-        }
-        cnt[0] = 1;
-
-        for (int i = 1; i < n; i++) {
-            cnt[i] = 1;
-            for (int j = 0; j < i; j++) {
-                if (a[j] < a[i] && cnt[i] <= cnt[j]) {
-                    cnt[i] = cnt[j] + 1;
+            int x = Integer.parseInt(br.readLine());
+            if (x == 0) {
+                if (pq.size() == 0) {
+                    bw.write("0\n");
+                } else {
+                    bw.write(pq.poll() + "\n");
                 }
+            } else {
+                pq.add(x);
             }
         }
-        int maxCnt = cnt[0];
-        for (int i = 1; i < n; i++) {
-            if (maxCnt < cnt[i])
-                maxCnt = cnt[i];
-        }
-        System.out.println(maxCnt);
+
+        bw.flush();
+        bw.close();
     }
 }
--- src/baekjoon/step21/q1655/Main.java
+++ src/baekjoon/step21/q1655/Main.java
@@ -14,40 +14,34 @@ import java.util.PriorityQueue;
  */
 public class Main {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
 
+        // 큰 수 pq
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        PriorityQueue<Integer> pqMinus = new PriorityQueue<>(Collections.reverseOrder());
+        // 작은 수 pq
+        PriorityQueue<Integer> pqReverse = new PriorityQueue<>(Collections.reverseOrder());
 
         for (int i = 0; i < n; i++) {
-            int x = Integer.parseInt(br.readLine());
-            if (x == 0) {
-                if (pq.size() == 0 && pqMinus.size() == 0) {
-                    bw.write("0\n");
-                } else {
-                    if (pq.size() == 0) {
-                        bw.write(pqMinus.poll() + "\n");
-                    } else if (pqMinus.size() == 0) {
-                        bw.write(pq.poll() + "\n");
-                    } else {
-                        if (pq.peek() >= pqMinus.peek() * -1) {
-                            bw.write(pqMinus.poll() + "\n");
-                        } else {
-                            bw.write(pq.poll() + "\n");
-                        }
-                    }
-                }
+            int num = Integer.parseInt(br.readLine());
+
+            if (pq.size() == pqReverse.size()) {
+                pqReverse.add(num);
             } else {
-                if (x > 0) {
-                    pq.add(x);
-                } else {
-                    pqMinus.add(x);
+                pq.add(num);
+            }
+
+            if (!pq.isEmpty() && !pqReverse.isEmpty()) {
+                if (pq.peek() < pqReverse.peek()) {
+                    int tmp = pq.poll();
+                    pq.add(pqReverse.poll());
+                    pqReverse.add(tmp);
                 }
             }
+            bw.write(pqReverse.peek() + "\n");
         }
-
         bw.flush();
         bw.close();
     }

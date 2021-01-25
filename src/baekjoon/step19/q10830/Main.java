ebd16efe6782a1f08820c2840eca9be28aa9acc8
tildev <tildevelove@gmail.com>
2021-01-25 20:26:20 +0900

    Solve the problem 10830 of s19

--- src/baekjoon/step19/q10830/Main.java
+++ src/baekjoon/step19/q10830/Main.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 /**
- * 행렬 제곱 - FAIL
+ * 행렬 제곱
  * <p>
  * 메모리, 시간 제한  - 256 MB,   1 초
  * 메모리, 시간      -  KB,  ms
@@ -13,19 +13,44 @@ import java.util.StringTokenizer;
  * @data 2021. 01. 24.
  */
 public class Main {
-    private static int n, b;
-    private static long[][] a, imp, result;
+    private static int n;
+    private static long b;
+    private static int[][] a, imp, result;
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     private static StringTokenizer st;
 
-    private static long[][] multi(long[][] a, long[][] b) {
-        imp = new long[n][n];
+    private static int[][] pow(long b) {
+        int[][] m, tmp = new int[n][n];
+
+        if (b == 1) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    tmp[i][j] = a[i][j] % 1000;
+                }
+            }
+            return tmp;
+        }
+        // b==1 이 아니면, b를 2로 나누어 재귀를 돌림 (분할)
+        m = pow(b / 2);
+        tmp = multi(m, m);
+
+        if (b % 2 == 1) {
+            m = multi(tmp, a);
+            return m;
+        } else {
+            return tmp;
+        }
+    }
+
+    private static int[][] multi(int[][] a, int[][] b) {
+        imp = new int[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 for (int k = 0; k < n; k++) {
                     imp[i][j] += (a[i][k] * b[k][j]);
                 }
+                imp[i][j] %= 1000;
             }
         }
         return imp;
@@ -33,29 +58,26 @@ public class Main {
 
 
     public static void main(String[] args) throws IOException {
-        st = new StringTokenizer(br.readLine());
+        // b의 범위가 크므로, long 값으로 받아야함. stringTokenizer 로 받아서는 안됨.
+        String[] sArr = br.readLine().split(" ");
+        n = Integer.parseInt(sArr[0]);
+        b = Long.parseLong(sArr[1]);
 
-        n = Integer.parseInt(st.nextToken());
-        b = Integer.parseInt(st.nextToken());
-
-        a = new long[n][n];
-        result = new long[n][n];
+        a = new int[n][n];
+        result = new int[n][n];
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
-                a[i][j] = Long.parseLong(st.nextToken());
-                result[i][j] = a[i][j];
+                a[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        for (int i = 0; i < b; i++) {
-            result = multi(result, a);
-        }
+        result = pow(b);
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                bw.write(result[i][j] % 1000 + " ");
+                bw.write(result[i][j] + " ");
             }
             bw.write("\n");
         }

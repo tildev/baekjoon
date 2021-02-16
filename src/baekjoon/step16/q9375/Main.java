bde4179c7d9cb9a56eced52c8b938da1acbe1380
tildev <tildevelove@gmail.com>
2021-02-16 20:46:58 +0900

    Edit format

--- src/baekjoon/step16/q9375/Main.java
+++ src/baekjoon/step16/q9375/Main.java
@@ -13,7 +13,7 @@ import java.util.StringTokenizer;
  * 메모리, 시간      - 11908 KB, 80 ms
  * <p>
  * 경우의 수 문제
- * <p>
+ *
  * @author tildev
  * @data 2021. 02. 15.
  */
@@ -26,25 +26,25 @@ public class Main {
     private static Map<String, Integer> map;
 
     public static void main(String[] args) throws IOException {
-    	t = Integer.valueOf(br.readLine());
-    	for (int i = 0; i < t; i++) {
-	    	n = Integer.valueOf(br.readLine());
-	    	map = new HashMap<>();
-	    	for (int j = 0; j < n; j++) {
-		    	st = new StringTokenizer(br.readLine());
-		
-		        st.nextToken();
-		        mapKey = st.nextToken();
-		        map.put(mapKey, map.getOrDefault(mapKey, 0) + 1);
-	    	}
-	    	
-	    	result = 1;
-	    	for (String key : map.keySet()) {
-	    		result *= (map.get(key) + 1);
-	    	}
+        t = Integer.valueOf(br.readLine());
+        for (int i = 0; i < t; i++) {
+            n = Integer.valueOf(br.readLine());
+            map = new HashMap<>();
+            for (int j = 0; j < n; j++) {
+                st = new StringTokenizer(br.readLine());
 
-	        bw.write((result - 1) + "\n");
-    	}
+                st.nextToken();
+                mapKey = st.nextToken();
+                map.put(mapKey, map.getOrDefault(mapKey, 0) + 1);
+            }
+
+            result = 1;
+            for (String key : map.keySet()) {
+                result *= (map.get(key) + 1);
+            }
+
+            bw.write((result - 1) + "\n");
+        }
 
         bw.flush();
         bw.close();
